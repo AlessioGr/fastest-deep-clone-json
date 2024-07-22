344a103b9fda379cd1a5e1133e3c33d6ac4c1812
Alessio Gravili <alessio@gravili.de>
2024-07-22 04:32:20 -0400

    chore: add very basic test and improve JSDocs

--- index.ts
+++ index.ts
@@ -8,10 +8,7 @@ export interface JsonObject {
 
 
 /**
- * A deepCopyObject implementation which only works for JSON objects and arrays, and is faster than
- * JSON.parse(JSON.stringify(inObject)): https://www.measurethat.net/Benchmarks/Show/31442/0/jsonstringify-vs-structuredclone-vs-simple-deepcopyobje
- *
- * This is not recursive and should thus be more memory efficient, due to less stack frames
+ * Deep clone a JSON object or array. Only works for JSON objects and arrays.
  */
 export const deepCloneJson = <T extends JsonObject>(inObject: T): T => {
     if (typeof inObject !== 'object' || inObject === null) {
@@ -24,7 +21,7 @@ export const deepCloneJson = <T extends JsonObject>(inObject: T): T => {
     const root = stack[0].target
 
     while (stack.length > 0) {
-        const { source, target }= stack.pop() as { source: JsonObject; target: any }
+        const { source, target } = stack.pop() as { source: JsonObject; target: any }
 
         for (const key in source) {
             const value = source[key]
---
+++ test/index.js
@@ -0,0 +1,17 @@
+import { deepCloneJson } from '../dist/index.js';
+
+const obj = { a: 1, b: { c: 2, d: [1, 2, { test: 'hello' }, 4] } };
+
+const cloned = deepCloneJson(obj);
+
+//Assert the cloned object is not the same as the original object
+if (cloned === obj) {
+  throw new Error('The cloned object is the same as the original object');
+}
+
+//Assert the cloned object is deeply equal to the original object
+if (JSON.stringify(cloned) !== JSON.stringify(obj)) {
+  throw new Error('The cloned object is not deeply equal to the original object');
+}
+
+console.log('Deep clone successful');
