10209d1e4b719ca414a339a22dda8fd14e259cb8
Alessio Gravili <alessio@gravili.de>
2024-07-22 04:33:43 -0400

    chore: add prettier

--- index.ts
+++ index.ts
@@ -3,36 +3,35 @@ export type JsonValue = JsonArray | JsonObject | boolean | null | number | strin
 export interface JsonArray extends Array<JsonValue> {}
 
 export interface JsonObject {
-    [key: string]: JsonValue
+  [key: string]: JsonValue
 }
-
 
 /**
  * Deep clone a JSON object or array. Only works for JSON objects and arrays.
  */
 export const deepCloneJson = <T extends JsonObject>(inObject: T): T => {
-    if (typeof inObject !== 'object' || inObject === null) {
-        return inObject
+  if (typeof inObject !== 'object' || inObject === null) {
+    return inObject
+  }
+
+  const stack: { source: JsonArray | JsonObject; target: any }[] = [
+    { source: inObject, target: Array.isArray(inObject) ? [] : {} },
+  ]
+  const root = stack[0].target
+
+  while (stack.length > 0) {
+    const { source, target } = stack.pop() as { source: JsonObject; target: any }
+
+    for (const key in source) {
+      const value = source[key]
+      if (typeof value === 'object' && value !== null) {
+        target[key] = Array.isArray(value) ? [] : {}
+        stack.push({ source: value, target: target[key] })
+      } else {
+        target[key] = value
+      }
     }
+  }
 
-    const stack: { source: JsonArray | JsonObject; target: any }[] = [
-        { source: inObject, target: Array.isArray(inObject) ? [] : {} },
-    ]
-    const root = stack[0].target
-
-    while (stack.length > 0) {
-        const { source, target } = stack.pop() as { source: JsonObject; target: any }
-
-        for (const key in source) {
-            const value = source[key]
-            if (typeof value === 'object' && value !== null) {
-                target[key] = Array.isArray(value) ? [] : {}
-                stack.push({ source: value, target: target[key] })
-            } else {
-                target[key] = value
-            }
-        }
-    }
-
-    return root
+  return root
 }
--- test/index.js
+++ test/index.js
@@ -1,17 +1,17 @@
-import { deepCloneJson } from '../dist/index.js';
+import { deepCloneJson } from '../dist/index.js'
 
-const obj = { a: 1, b: { c: 2, d: [1, 2, { test: 'hello' }, 4] } };
+const obj = { a: 1, b: { c: 2, d: [1, 2, { test: 'hello' }, 4] } }
 
-const cloned = deepCloneJson(obj);
+const cloned = deepCloneJson(obj)
 
 //Assert the cloned object is not the same as the original object
 if (cloned === obj) {
-  throw new Error('The cloned object is the same as the original object');
+  throw new Error('The cloned object is the same as the original object')
 }
 
 //Assert the cloned object is deeply equal to the original object
 if (JSON.stringify(cloned) !== JSON.stringify(obj)) {
-  throw new Error('The cloned object is not deeply equal to the original object');
+  throw new Error('The cloned object is not deeply equal to the original object')
 }
 
-console.log('Deep clone successful');
+console.log('Deep clone successful')
